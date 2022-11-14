b55c3ff530092e82d9dd5600d0fd28a6082f9dbe
Kumoichi <minimi_ken1@yahoo.co.jp>
2022-11-13 20:52:02 -0800

    add timer

--- app/src/main/java/com/example/savestudy/DBHelper.java
+++ app/src/main/java/com/example/savestudy/DBHelper.java
@@ -9,19 +9,47 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
+    private static final String ALERT_DATABASE = "Userdata.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "Userdetails";
+    private static final String SUBJECT_ID = "subject";
+    private static final String TODO_ID = "toDoTask";
+    private static final String TIME_ID = "stuTime";
+    private static final String COUNT_ID = "count";
+
+    private static final String TABLE_NAME2 = "barchart_table";
+    private static final String KEY_ID = "id";
+    private static final String DATE = "date";
+    private static final String yAXIS = "yaxis";
+
+
+    private static final String CREATE_ALERT_TYPE="CREATE TABLE "
+            + TABLE_NAME + " ( "
+            + SUBJECT_ID +" TEXT,"
+            + TODO_ID +" TEXT,"
+            + TIME_ID +" TEXT,"
+            + COUNT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT)";
+
+    private static final String CREATE_ALERT_INFORMATION="CREATE TABLE "
+            + TABLE_NAME2 + " ( "
+            + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + DATE + " TEXT,"
+            + yAXIS + " TEXT)";
+
+
     public DBHelper(@Nullable Context context) {
-        super(context, "Userdata.db",null,1);
+        super(context, ALERT_DATABASE,null,DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create Table Userdetails(subject Text , toDoTask TEXT , stuTime TEXT, count INTEGER primary key) ");
+        sqLiteDatabase.execSQL(CREATE_ALERT_TYPE);
+        sqLiteDatabase.execSQL(CREATE_ALERT_INFORMATION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("drop Table if exists Userdetails");
-
     }
 
     public Boolean insertuserData(String subject, String toDoTask, String stuTime, int count) {
@@ -31,7 +59,7 @@ public class DBHelper extends SQLiteOpenHelper {
         contentValues.put("toDoTask", toDoTask);
         contentValues.put("stuTime", stuTime);
         contentValues.put("count", count);
-        long result = db.insert("Userdetails", null, contentValues);
+        long result = db.insert(TABLE_NAME, null, contentValues);
         if (result == -1) {
             return false;
         } else {
@@ -73,4 +101,7 @@ public class DBHelper extends SQLiteOpenHelper {
         Cursor cursor = db.rawQuery("Select * from Userdetails", null);
         return cursor;
     }
+
+
+    //for the Graph view
 }
